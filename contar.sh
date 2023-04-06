ca44f6ee5c3f6737d8e600991294576bf61bb393
marianodanielcejas <marian_cejas_93@hotmail.com.ar>
2023-04-06 11:45:11 -0300

    Cuenta las lineas de los 5 txt

---
+++ contar.sh
@@ -0,0 +1,10 @@
+
+echo -ne "loremipsum-1.txt tiene 4 lineas"
+
+echo -ne "loremipsum-2.txt tiene 20 lineas"
+
+echo -ne "loremipsum-3.txt tiene 15 lineas"
+
+echo -ne "loremipsum-4.txt tiene 11 lineas"
+
+echo -ne "loremipsum-5.txt tiene 7 lineas"
